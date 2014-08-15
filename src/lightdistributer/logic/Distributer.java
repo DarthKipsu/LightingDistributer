45e8e2dabca21e1510d880c71627faf9504b4914
Verna Koskinen <darth.kipsu@gmail.com>
2014-08-15 11:20:46 +0300

    add calculator for needed columns, without restricted areas

--- src/lightdistributer/logic/Distributer.java
+++ src/lightdistributer/logic/Distributer.java
@@ -1,14 +1,26 @@
 
 package lightdistributer.logic;
 
+import java.util.List;
+import lightdistributer.domain.StakeInterval;
 import lightdistributer.domain.Street;
 
 public class Distributer {
 	private Street street;
+	private List<StakeInterval> stakeIntervals;
 
 	public Distributer(Street street) {
 		this.street = street;
+		stakeIntervals = street.getStakeIntervals();
 	}
 
+	public int countNeededColumns() {
+		double columns = 0.0;
+		for (StakeInterval interval : stakeIntervals) {
+			columns += (interval.getEnd() - interval.getBeginning()) / (double) interval.getSmax();
+		}
+		columns = Math.ceil(columns) + 1;
+		return (int) columns;
+	}
 	
 }
---
+++ test/DistributerTest.java
@@ -0,0 +1,56 @@
+
+import lightdistributer.domain.Street;
+import lightdistributer.logic.Distributer;
+import org.junit.After;
+import org.junit.AfterClass;
+import static org.junit.Assert.assertEquals;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+public class DistributerTest {
+	
+	public DistributerTest() {
+	}
+	
+	private Street street = new Street(50);
+	private Distributer distributer = new Distributer(street);	
+	
+	@BeforeClass
+	public static void setUpClass() {
+	}
+	
+	@AfterClass
+	public static void tearDownClass() {
+	}
+	
+	@Before
+	public void setUp() {
+		street.addStraightSection(100, true);
+		street.addOutsideCurve(280, true, 500);
+		street.addInsideCurve(340, true, 100);
+		street.addStraightSection(400, true);
+	}
+	
+	@After
+	public void tearDown() {
+	}
+
+	@Test
+	public void countsNeededColumns() {
+		int columnCount = distributer.countNeededColumns();
+		int expected = 10;
+
+		assertEquals(expected, columnCount);
+	}
+	
+	@Test
+	public void countsNeededColumns2() {
+		street.addOutsideCurve(650, true, 350);
+		int columnCount = distributer.countNeededColumns();
+		int expected = 16;
+
+		assertEquals(expected, columnCount);
+	}
+	
+}
