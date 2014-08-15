f8a5bc7fee16b65912913ea6c718b0e34b65c9e0
Verna Koskinen <darth.kipsu@gmail.com>
2014-08-15 13:05:40 +0300

    add column spacing smoothing factor

--- src/lightdistributer/logic/Distributer.java
+++ src/lightdistributer/logic/Distributer.java
@@ -8,19 +8,28 @@ import lightdistributer.domain.Street;
 public class Distributer {
 	private Street street;
 	private List<StakeInterval> stakeIntervals;
+	private List<Integer> stakes;
 
 	public Distributer(Street street) {
 		this.street = street;
 		stakeIntervals = street.getStakeIntervals();
 	}
 
+	private double getSmoothingFactor() {
+		return countExactColumns() / countNeededColumns();
+	}
+	
 	public int countNeededColumns() {
+		int columns = (int) Math.ceil(countExactColumns());
+		return columns;
+	}
+
+	private double countExactColumns() {
 		double columns = 0.0;
 		for (StakeInterval interval : stakeIntervals) {
 			columns += (interval.getEnd() - interval.getBeginning()) / (double) interval.getSmax();
 		}
-		columns = Math.ceil(columns) + 1;
-		return (int) columns;
+		return columns + 1;
 	}
-	
+
 }
